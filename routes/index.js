0d891cdfb956a07627a6b155fd1b33946e1bc6ed
Haoyu Huang <huanghaoyu1@gmail.com>
2020-09-17 18:11:55 -0700

    index.js error handling

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,9 @@ function isValidUsername(username) {
   return false;
 }
 
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
   passport.authenticate("local", { session: false }, (err, user, info) => {
-    if (err) return res.send(err);
+    if (err) return next(err);
 
     if (!user) {
       // username exists, password incorrect, prompt user to re-enter
@@ -32,10 +32,9 @@ router.post("/", (req, res) => {
           return res.status(200).send("create new user?");
         } else {
           // prompt user to re-enter
-          info.message = "Username does not comply to rules";
           console.log(info.message);
           return res.status(400).json({
-            error: "Username does not comply to rules",
+            error: "Username is reserved",
           });
         }
       }
@@ -44,7 +43,7 @@ router.post("/", (req, res) => {
     // user authenticated
     console.log("authenticated");
     req.logIn(user, { session: false }, (err) => {
-      if (err) return res.status(500).end();
+      if (err) return next(err);
 
       // TODO: generate jwt and return it in response
       return res.redirect("/");
@@ -52,7 +51,7 @@ router.post("/", (req, res) => {
   })(req, res);
 });
 
-router.post("create-user", (req, res) => {
+router.post("/create-user", (req, res, next) => {
   const { username, password } = req.body;
 
   // create new user and save to db
@@ -62,37 +61,16 @@ router.post("create-user", (req, res) => {
     hash: hashAndSalt.hash,
     salt: hashAndSalt.salt,
   });
-  newUser.save().then((user) => {
-    console.log(user);
-  });
-  res.redirect("/welcome");
+
+  newUser
+    .save()
+    .then((user) => {
+      console.log(user);
+      res.status(201).send("success");
+    })
+    .catch((err) => next(err));
 });
 
-//Old session authenticate logic
-
-// router.get('/main', (req, res) => {
-//     if(req.session.passport === undefined){
-//         res.redirect('/')
-//     }
-//     else {
-//             const id = (req.session.passport.user)
-
-//         if (req.isAuthenticated()) {
-//             User.findOne({ _id: id })
-//             .then((user) => {
-//                 var username = user.username
-//                 res.render('index',{username : username})
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//             });
-
-//         } else {
-//             res.send('<h1>You are not authenticated</h1><p><a href="/">Login</a></p>');
-//         }
-//     }
-
-// });
 
 //new JWT TO DO #
 router.get("/main", (req, res) => {
@@ -102,12 +80,6 @@ router.get("/main", (req, res) => {
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
-});
-router.get("/wrong-password", (req, res) => {
-  res.send("<h1>Login failed</h1>");
-});
-router.get("/username-taken", (req, res) => {
-  res.send("<h1>Username already taken.</h1>");
 });
 
 module.exports = router;
