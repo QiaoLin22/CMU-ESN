51d18544da14ad26f3e420d0514e08883008e9b8
Mimi Shih <shihm042@newschool.edu>
2020-10-04 17:30:09 -0400

    update directory and chat views

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,16 @@ const LoginController = require('../controllers/loginController');
 const UserController = require('../controllers/userController');
 
 router.get('/main', requireAuth, (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: 'You are successfully authenticated to this route!',
-  });
+  // res.status(200).json({
+  //   success: true,
+  //   msg: 'You are successfully authenticated to this route!',
+  // });
+  res.render('main');
 });
+
+// router.get('/main/public', requireAuth, (req, res) => {
+//   res.render('chat');
+// });
 
 router.get('/', (req, res) => {
   res.render('index');
