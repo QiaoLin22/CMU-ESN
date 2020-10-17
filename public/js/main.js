0ebb6466ca91c47b2cda56f28f25eeb9a569d73e
Mimi Shih <meitsens@andrew.cmu.edu>
2020-10-16 22:19:24 -0400

    share status backend.
    Co-Authored-By: Meitsen Shih <shihm042@newschool.edu>
    Co-Authored-By: Qiao Lin <qiaolin@andrew.cmu.edu>

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,11 @@
 const socket = io();
 
 const logoutBtn = $('#logoutBtn');
-const username = $('#username-data').val();
-const ok = $(".status-btn:nth-child(1)");
-const help = $(".status-btn:nth-child(2)");
-const emergency = $(".status-btn:nth-child(3)");
-const undefined = $(".status-btn:nth-child(4)");
-
-
+const username = $('#username-data');
+const ok = $('.status-btn:nth-child(1)');
+const help = $('.status-btn:nth-child(2)');
+const emergency = $('.status-btn:nth-child(3)');
+const na = $('.status-btn:nth-child(4)');
 
 function getGetOptions() {
   return {
@@ -19,14 +17,25 @@ function getGetOptions() {
   };
 }
 
-function outputUser(data, online) {
+function outputUser(data, online, status) {
+  let icon = '';
+  if (status === 'OK') {
+    icon = '<i class="far fa-check-circle ml-1" style="color: #18b87e"></i>';
+  } else if (status === 'Help') {
+    icon = '<i class="fas fa-info-circle ml-1" style="color: #ffd500"></i>';
+  } else if (status === 'Emergency') {
+    icon = '<i class="fas fa-first-aid ml-1" style="color: #fb5252"></i>';
+  } else if (status === undefined || status === 'Undefined') {
+    icon = '<i class="far fa-question-circle ml-1" style="color: #d8d8d8"></i>';
+  }
+
   const user = document.createElement('div');
   user.classList.add('online-item');
   if (online) {
-    user.innerHTML = `<li class="list-group-item list-group-item-action">${data.username+' '+data.status}</li>`;
+    user.innerHTML = `<li class="list-group-item list-group-item-action">${`${`${data.username} ${icon}`}`}</li>`;
     $('#online-list').append(user);
   } else {
-    user.innerHTML = `<li class="list-group-item list-group-item-action offline-list-item">${data.username}</>`;
+    user.innerHTML = `<li class="list-group-item list-group-item-action offline-list-item">${`${`${data.username} ${icon}`}`}</>`;
     $('#offline-list').append(user);
   }
 }
@@ -38,7 +47,7 @@ function retrieveUsers() {
     })
     .then((data) => {
       data.users.forEach((user) => {
-        outputUser(user, user.online);
+        outputUser(user, user.online, user.status);
       });
     })
     .catch((e) => {
@@ -71,26 +80,24 @@ logoutBtn.on('click', () => {
   });
 });
 
-function updateStatus(status,username) {
+function updateStatus(status) {
   status.on('click', () => {
     let newStatus = {
-      username: username,
-      status:status.text()
-    }
+      username: username.val(),
+      status: status.text(),
+    };
     fetch('/api/users', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(newStatus)
+      body: JSON.stringify(newStatus),
     }).catch((e) => {
       console.log(e);
     });
-  })
+  });
 }
-updateStatus(ok,username)
-updateStatus(help,username)
-updateStatus(emergency,username)
-updateStatus(undefined,username)
-
-
+updateStatus(ok);
+updateStatus(help);
+updateStatus(emergency);
+updateStatus(na);
