866872c7a152413e3dd70296ec8804e01b969bf2
Haoyu Huang <huanghaoyu1@gmail.com>
2020-10-20 20:58:44 -0700

    fixed username out-of-order bug

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const User = require('../models/user')
+const User = require('../models/user');
 
 let mongoServer;
 
 beforeAll(async () => {
-    mongoServer = new MongoMemoryServer();
-    const mongoUri = await mongoServer.getUri();
-    await mongoose.connect(mongoUri,(err) => {
-      if (err) console.error(err);
-    });
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose.connect(mongoUri, (err) => {
+    if (err) console.error(err);
   });
-  
-  afterAll(async () => {
-    await mongoose.disconnect();
-    await mongoServer.stop();
-  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-    test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
-    });
-
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
