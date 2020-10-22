1a025b062b1dc025144fa98edb7e87e204f86267
Haoyu Huang <huanghaoyu1@gmail.com>
2020-10-21 22:08:40 -0700

    added test in circleci config

--- tests/user.test.js
+++ tests/user.test.js
@@ -35,21 +35,3 @@ describe('use case join community', () => {
     expect(actual[0].status).toEqual(expected[0].status);
   });
 });
-
-
-    const actual = await User.find(
-      { username: 'john' },
-      { _id: 0, __v: 0, timestamp: 0 }
-    );
-
-    console.log(actual);
-    const expected = [
-      {
-        username: 'john',
-        hash: '001',
-        salt: '1110',
-        online: false,
-        status: 'Undefined',
-      },
-    ];
-    expect(actual).toEqual(expected);
