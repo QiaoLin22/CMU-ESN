78558532a927e29c273aa89340e06cf7e1f0659c
Qiao Lin <qiaolin@andrew.cmu.edu>
2020-10-22 23:12:15 -0700

    share status backend.
    Co-Authored-By: Meitsen Shih <shihm042@newschool.edu>
    Co-Authored-By: Qiao Lin <qiaolin@andrew.cmu.edu>

--- tests/user.test.js
+++ tests/user.test.js
@@ -13,7 +13,7 @@ describe('use case join community', () => {
   it('create new user successfully', async () => {
     await createNewUser('John', '001', '1110');
 
-    const actual = await User.find(
+    const actual = await User.findOne(
       { username: 'John' },
       { _id: 0, __v: 0, timestamp: 0 }
     );
@@ -31,14 +31,37 @@ describe('use case join community', () => {
         ],
       },
     ];
+    expect(actual.username).toEqual(expected[0].username);
+    expect(actual.hash).toEqual(expected[0].hash);
+    expect(actual.salt).toEqual(expected[0].salt);
+    expect(actual.online).toEqual(expected[0].online);
+    expect(actual.statusArray[0].status).toEqual(
+      expected[0].statusArray[0].status
+    );
+  });
+
+  it('Find user by username successfully', async () => {
+    const actual = await findUserByUsername('John');
+    console.log(actual)
+    const expected = [
+      {
+        username: 'John',
+        hash: '001',
+        salt: '1110',
+        online: false,
+        statusArray: [
+          {
+            status: 'Undefined',
+          },
+        ],
+      },
+    ];
     expect(actual[0].username).toEqual(expected[0].username);
-    expect(actual[0].hash).toEqual(expected[0].hash);
-    expect(actual[0].salt).toEqual(expected[0].salt);
     expect(actual[0].online).toEqual(expected[0].online);
     expect(actual[0].statusArray[0].status).toEqual(
       expected[0].statusArray[0].status
     );
-  });
+  })
 
   it('Update status successfully', async() => {
     await updateStatusIcon('John','OK');
@@ -59,17 +82,17 @@ describe('use case join community', () => {
         ],
       },
     ];
-    const actual = await User.find(
+    const actual = await User.findOne(
       { username: 'John' },
       { _id: 0, __v: 0, timestamp: 0 }
     );
     
-    expect(actual[0].username).toEqual(expected[0].username);
-    expect(actual[0].hash).toEqual(expected[0].hash);
-    expect(actual[0].salt).toEqual(expected[0].salt);
-    expect(actual[0].online).toEqual(expected[0].online);
-    expect(actual[0].status).toEqual(expected[0].status);
-    expect(actual[0].statusArray[1].status).toEqual(
+    expect(actual.username).toEqual(expected[0].username);
+    expect(actual.hash).toEqual(expected[0].hash);
+    expect(actual.salt).toEqual(expected[0].salt);
+    expect(actual.online).toEqual(expected[0].online);
+    expect(actual.status).toEqual(expected[0].status);
+    expect(actual.statusArray[1].status).toEqual(
       expected[0].statusArray[1].status
     );
   })
