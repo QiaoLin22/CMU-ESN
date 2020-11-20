f0466a80ff56537b2e210fb9eb2ceeda823e3e0d
Qiao Lin <qiaolin@andrew.cmu.edu>
2020-11-19 17:01:49 -0800

    update map controller

--- controllers/map-controller.js
+++ controllers/map-controller.js
@@ -8,25 +8,23 @@ const {
 class MapController {
   static updateLocation(req, res) {
     const { username, longitude, latitude } = req.body;
-    updateUserLocation(username, longitude, latitude)
-      .then(() => {
-        req.app.get('io').emit('updateMap');
-        res.status(200).send({ message: 'success' });
-      })
-      .catch((err) => next(err));
+    updateUserLocation(username, longitude, latitude).then(() => {
+      req.app.get('io').emit('updateMap');
+      res.status(200).send({ message: 'success' });
+    });
   }
 
   static retrieveLocations(req, res) {
-    retrieveUserLocations()
-      .then((locations) => res.status(200).json(locations))
-      .catch((err) => next(err));
+    retrieveUserLocations().then((locations) =>
+      res.status(200).json(locations)
+    );
   }
 
   static retrieveLocation(req, res) {
     const { username } = res.locals;
-    retrieveUserLocation(username)
-      .then((location) => res.status(200).json(location))
-      .catch((err) => next(err));
+    retrieveUserLocation(username).then((location) =>
+      res.status(200).json(location)
+    );
   }
 
   static deleteLocation(req, res) {
