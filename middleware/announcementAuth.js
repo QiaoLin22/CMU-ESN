d62cae31a450f1795326dde321d0d754ab59d5b2
Mimi Shih <meitsens@andrew.cmu.edu>
2020-12-06 22:31:25 -0500

    update validation.
    Co-Authored-By: Qiao Lin <qiaolin@andrew.cmu.edu>

--- middleware/announcementAuth.js
+++ middleware/announcementAuth.js
@@ -2,14 +2,16 @@ const User = require('../models/user');
 
 const authenticateAnnouncement = (req, res, next) => {
   const { username } = res.locals;
-  User.getUserPrivilege(username).then((result) => {
-    if (result.privilegeLevel === 'Coordinator') {
-      next();
-    }
-  });
-  // .catch((e) => {
-  //   res.status(400).json({ error: e });
-  // });
+  User.getUserPrivilege(username)
+    .then((result) => {
+      if (result.privilegeLevel === 'Coordinator') {
+        next();
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      // res.status(400).json({ error: e });
+    });
 };
 
 module.exports = { authenticateAnnouncement };
